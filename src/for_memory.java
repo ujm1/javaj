3a786271095775693bb0afe247cb3f6778f003d8
ujm1 <woakswoaks@gmail.com>
2023-11-01 17:36:38 +0900

    자바의정석

---
+++ src/for_memory.java
@@ -0,0 +1,16 @@
+public class for_memory {
+    public static void main(String[] args) {
+        System.out.println("'");
+        System.out.println('\'');
+        System.out.println("\'");
+        System.out.println("abc\n123\tabc\b456");
+        System.out.println("\"Hello\"");
+        double pi = 3.14;
+        System.out.println(pi);
+        int a = 100;
+        int b = 10;
+        System.out.println(a * b / a);
+        System.out.println(a / b * a); // * ; 사이엔 순서 x
+//        char var1="A"; <- char는 ' ' 만 . " "는 문자열.
+    }
+}
